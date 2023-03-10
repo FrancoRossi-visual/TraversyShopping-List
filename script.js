ce608a3cfc0ff95fa87ac535fe7c41ef3156a70e
Fran <francorossi.visual@gmail.com>
2023-03-09 21:36:04 -0300

    added remove and status check

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-// shoping list app
-asdf;
 // ui items
 const itemFormUI = document.getElementById('item-form');
 const itemInputUI = document.getElementById('item-input');
 const submitUI = document.querySelector('.btn');
 const itemListUI = document.getElementById('item-list');
+const clearAllUI = document.getElementById('clear');
+const itemFilterUI = document.getElementById('filter');
 
-// components:
+// ui state:
+function uiState() {
+  itemListUI.firstElementChild
+    ? ((clearAllUI.style.display = 'inline-block'),
+      (itemFilterUI.style.display = 'inline-block'))
+    : ((clearAllUI.style.display = 'none'),
+      (itemFilterUI.style.display = 'none'));
+}
+
+uiState();
+// add and remove items:
 
 // create li w/ button n' icon ; add item:
 function addItem(e) {
@@ -26,6 +36,8 @@ function addItem(e) {
 
   // append item to item list
   itemListUI.appendChild(itemLi);
+
+  uiState();
 }
 
 // create button w/ icon:
@@ -37,6 +49,7 @@ function createButton(buttonClass) {
   button.appendChild(icon);
   return button;
 }
+
 // create icon:
 function createIcon(iconClass) {
   const icon = document.createElement('i');
@@ -44,5 +57,30 @@ function createIcon(iconClass) {
   return icon;
 }
 
+// delete item
+function deleteItem(e) {
+  if (e.target.tagName === 'I') {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+  uiState();
+}
+
+// clear all items
+function clearAll() {
+  if (confirm('Are you sure you wish to clear all items?'))
+    while (itemListUI.firstChild) {
+      itemListUI.removeChild(itemListUI.firstChild);
+    }
+  uiState();
+}
+
+// filter
+function filter() {}
+
 // event listeners
 itemFormUI.addEventListener('submit', addItem);
+itemListUI.addEventListener('click', deleteItem);
+clearAllUI.addEventListener('click', clearAll);
+itemFilterUI.addEventListener('keyup', filter);
